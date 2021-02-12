34b11e2e62ab3da7256d9cad0697f8d06eb1c575
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-12 10:55:11 -0800

    create append function

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './services/currency-service.js';
-
+const appendCurrencyKeys = (exchangeRate) => {
+  $.each(exchangeRate, function(key, value) {
+    $('#currencyType').append($('<option></option>').attr('value', value).text(key));
+  })
+}
 const clearFields = () => {
   $('#currencyAmount').val("");
   $('.show-errors').val("");
@@ -11,30 +15,29 @@ const clearFields = () => {
 const displayErrors = (error) => {
   $('.show-errors').text(`${error}`);
 };
-const displayCurrencyConversion = (exchangeRate, amount) => {
-  const convertedAmount = (amount * exchangeRate.AED);
+const displayCurrencyConversion = (currency, amount) => {
+  const convertedAmount = (amount * currency);
   $('.show-currency').text(`Conversion is ${convertedAmount}`);
 };
 
 $(document).ready(function() {
-  $('#currencyRate').on('click', function() {
-    let amount = $('#currencyAmount').val();
-    clearFields();
-    CurrencyService.getCurrency()
-      .then(function(currencyResponse) {
-        console.log(currencyResponse)
-        if (currencyResponse instanceof Error) {
-          throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
-        }
-        const exchangeRate = currencyResponse.conversion_rates;
-        $.each(exchangeRate, function(key, value) {
-          $('#currencyType').append($('<option></option>').attr('value', value).text(key));
-        })
-    
-        displayCurrencyConversion(exchangeRate, amount);
-      })
-      .catch(function(error) {
-        displayErrors(error.message);
-      });
+  CurrencyService.getCurrency()
+    .then(function(currencyResponse) {
+      if (currencyResponse instanceof Error) {
+        throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
+      }
+      const exchangeRate = currencyResponse.conversion_rates;
+      appendCurrencyKeys(exchangeRate);
+  
+    })
+    .catch(function(error) {
+      displayErrors(error.message);
+    });
+    $('#currencyRate').on('click', function() {
+      let amount = $('#currencyAmount').val();
+      let currency = parseFloat($('#currencyType').val());
+      console.log(currency)
+      displayCurrencyConversion(currency, amount);
+      clearFields();
   });
 });
