095c23155057107e2d01a1c48559c135a06ea31e
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-12 12:28:52 -0800

    handle commas used as amount delimiter

--- src/main.js
+++ src/main.js
@@ -8,9 +8,6 @@ const appendCurrencyKeys = () => {
   const countryKeys = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLP","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","FOK","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KID","KMF","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TVD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XCD","XDR","XOF","XPF","YER","ZAR","LYD","SSP","SYP","VES","YER","ZMW"];
   countryKeys.forEach(key =>
     $('#fromCurrencyType, #toCurrencyType').append($('<option></option>').attr('value', key).text(key)));
-  // $.each(exchangeRate, function(key, value) {
-  //   $('#currencyType').append($('<option></option>').attr('value', value).text(key));
-  // });
 };
 const displayCurrencyConversion = (amount, conversionRate) => {
   const convertedAmount = (amount * conversionRate);
@@ -27,19 +24,18 @@ const displayErrors = (error) => {
 $(document).ready(function() {
   appendCurrencyKeys();
   $('#currencyRate').on('click', function() {
-    let amount = $('#currencyAmount').val();
+    let amount = parseFloat($('#currencyAmount').val());
+    console.log(typeof amount)
     let fromCurrency = $('#fromCurrencyType').val();
     let toCurrency = $('#toCurrencyType').val();
-    console.log(fromCurrency)
-    console.log(toCurrency)
     clearFields();
     CurrencyService.getCurrency(fromCurrency, toCurrency)
       .then(function(currencyResponse) {
         if (currencyResponse instanceof Error) {
           throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
         }
-        console.log(currencyResponse)
         const conversionRate = currencyResponse.conversion_rate;
+        console.log(typeof conversionRate)
         displayCurrencyConversion(amount, conversionRate);
       })
       .catch(function(error) {
