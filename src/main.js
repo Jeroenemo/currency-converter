afbab53e5fa9afc5e95e5a78fd527d960c3c634e
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-12 09:42:33 -0800

    add conversion functionality

--- src/main.js
+++ src/main.js
@@ -6,22 +6,30 @@ import CurrencyService from './services/currency-service.js';
 const clearFields = () => {
   $('#currency').val("");
   $('.show-errors').val("");
-
-}
+};
+const displayErrors = (error) => {
+  $('.show-errors').text(`${error}`);
+};
+const displayCurrencyConversion = (exchangeRate, amount) => {
+  // console.log(exchangeRate)
+  const convertedAmount = (amount * exchangeRate.AED);
+  $('.show-currency').text(`Conversion is ${convertedAmount}`);
+};
 $(document).ready(function() {
   $('#currencyRate').on('click', function() {
     let amount = $('#currency').val();
     clearFields();
     CurrencyService.getCurrency()
       .then(function(currencyResponse) {
+        console.log(currencyResponse)
         if (currencyResponse instanceof Error) {
-          throw Error(`ExchangeRate API error: ${currencyResponse.message}`)
+          throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
         }
         const exchangeRate = currencyResponse.conversion_rates;
-        displayCurrencyConversion(exchangeRate, amount)
+        displayCurrencyConversion(exchangeRate, amount);
       })
       .catch(function(error) {
-        displayErrors(error.message)
-      })
-  })
-})
+        displayErrors(error.message);
+      });
+  });
+});
