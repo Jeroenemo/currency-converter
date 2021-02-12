a9bced83cf939c6fa8a155a4198f3d69d7f7b354
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-12 11:54:50 -0800

    append keys in array to select

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './services/currency-service.js';
 
-const keys = [AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTN,BWP,BYN,BZD,CAD,CDF,CHF,CLP,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,FOK,GBP,GEL,GGP,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JMD,JOD,JPY,KES,KGS,KHR,KID,KMF,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRU,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,SSP,STN,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TVD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VES,VND,VUV,WST,XAF,XCD,XDR,XOF,XPF,YER,ZAR,LYD,SSP,SYP,VES,YER,ZMW]
-const appendCurrencyKeys = (exchangeRate) => {
-  $.each(exchangeRate, function(key, value) {
-    $('#currencyType').append($('<option></option>').attr('value', value).text(key));
-  });
+const appendCurrencyKeys = () => {
+  const countryKeys = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLP","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","FOK","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KID","KMF","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TVD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XCD","XDR","XOF","XPF","YER","ZAR","LYD","SSP","SYP","VES","YER","ZMW"]
+  countryKeys.forEach(key =>
+    $('#currencyType').append($('<option></option>').attr('value', key).text(key)));
+  // $.each(exchangeRate, function(key, value) {
+  //   $('#currencyType').append($('<option></option>').attr('value', value).text(key));
+  // });
 };
 const clearFields = () => {
   $('#currencyAmount').val("");
@@ -23,22 +25,23 @@ const displayCurrencyConversion = (currency, amount) => {
 };
 
 $(document).ready(function() {
-  CurrencyService.getCurrency()
-    .then(function(currencyResponse) {
-      if (currencyResponse instanceof Error) {
-        throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
-      }
-      const exchangeRate = currencyResponse.conversion_rates;
-      appendCurrencyKeys(exchangeRate);
-  
-    })
-    .catch(function(error) {
-      displayErrors(error.message);
-    });
+  appendCurrencyKeys();
   $('#currencyRate').on('click', function() {
     let amount = $('#currencyAmount').val();
     let currency = parseFloat($('#currencyType').val());
     displayCurrencyConversion(currency, amount);
     clearFields();
+    CurrencyService.getCurrency()
+      .then(function(currencyResponse) {
+        if (currencyResponse instanceof Error) {
+          throw Error(`ExchangeRate API error: ${currencyResponse.message}`);
+        }
+        const exchangeRate = currencyResponse.conversion_rates;
+        appendCurrencyKeys(exchangeRate);
+    
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   });
 });
