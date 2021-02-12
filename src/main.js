c15ae3320cdb2424e8475bb80f408253315979ee
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-12 09:15:20 -0800

    call service

--- src/main.js
+++ src/main.js
@@ -2,3 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './services/currency-service.js';
+
+$(document).ready(function() {
+  $('#currencyRate').on('click', function() {
+    let amount = $('#currency').val();
+    CurrencyService.getCurrency()
+      .then(function(currencyResponse) {
+        if (currencyResponse instanceof Error) {
+          throw Error(`ExchangeRate API error: ${currencyResponse.message}`)
+        }
+        const exchangeRate = currencyResponse.conversion_rates;
+        displayCurrencyConversion(exchangeRate, amount)
+      })
+      .catch(function(error) {
+        displayErrors(error.message)
+      })
+  })
+})
